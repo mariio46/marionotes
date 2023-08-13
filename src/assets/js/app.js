ca678551fcd4c2a7c4597d3ab98c30bba3e00f36
Mario <mariomad2296@gmail.com>
2023-08-14 03:36:34 +0800

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,13 @@
+window.onscroll = function () {
+  const header = document.querySelector("nav");
+  const nav = document.querySelector("navigation");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("nav-fixed");
+    nav.classList.add("transparent-nav");
+  } else {
+    header.classList.remove("nav-fixed");
+    nav.classList.remove("transparent-nav");
+  }
+};
